8fb591009031718978f2e320d3adc715eefb7fcd
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-17 01:50:56 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,21 @@
-'use strict';
+class Question {
+  constructor(question, id){
+      this.constructor._All = [];
+      this.save;
+  }
+ 
+  static All() {
+    return this._All;
+  }
+
+  save() {
+  this.constructor._All.push(this)
+}
+ 
+  static Find(id){
+    this.All().filter(function(question){
+    return question.question === id;
+    })
+  }
+}
+
