2d10b320cb3f9c9aa113cf6601d52f6750b79e04
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-17 01:58:25 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,22 @@
 'use strict';
+
+class Question {
+  constructor(content){
+      this.content = content;
+      this.save();
+  }
+
+  save() {
+    this.constructor._All.push(this)
+}
+
+  static All() {
+    return this._All;
+  }
+
+  static Find(id){
+    return this.All()[id - 1]
+  }
+}
+
+Question._All = []
